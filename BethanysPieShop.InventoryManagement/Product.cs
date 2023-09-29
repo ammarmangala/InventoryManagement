d61a591de20a25ae850cd6d5eb61a42f1ec842fe
ammar <ammar@AMMAR>
2023-09-29 23:42:55 +0200

    Add project files.

---
+++ BethanysPieShop.InventoryManagement/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.InventoryManagement
+{
+    public class Product
+    {
+        private int _id;
+        private string _name = string.Empty;
+        private string? _description;
+
+        private int _maxItemsInStock = 0;
+
+    }
+}
